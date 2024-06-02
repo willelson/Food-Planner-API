015368d0a04691cedc35528054ecbabbb0801ab0
Will Elson <elson594@gmail.com>
2024-06-02 05:50:55 +0100

    Fix typo

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True)
     username = Column(String, unique=True, index=True)
-    fill_name = Column(String)
+    full_name = Column(String)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
