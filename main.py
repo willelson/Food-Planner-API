7f676afc70f512cd14e1321bf52495e867dec3c0
Will Elson <elson594@gmail.com>
2024-10-07 17:00:47 +0200

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, engine
 from routers.auth import router as auth_router
@@ -14,6 +15,14 @@ load_dotenv()
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(recipes_router)
